f2dba65398e467bd79fb5940eac3de7135b842a2
Aubrey <aubrey.goodman@gmail.com>
2012-10-17 12:13:44 -0400

    workflow is fixed now, i think

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,8 +12,11 @@ class AnswersController < ApplicationController
   
   def create
     @answer = Answer.new(params[:answer])
-    @answer.save
-    respond_with(@answer)
+    if @answer.save
+      redirect_to action: :index
+    else
+      respond_with(@answer)
+    end
   end
   
   def show
